f8319a68efef595d2433e632f571fbbbb05da8b1
hileydson <hileydson@hotmail.com>
2026-02-20 19:58:03 -0300

    Bugs

--- red-valve/scripts/enemies/thecobalthusker.gd
+++ red-valve/scripts/enemies/thecobalthusker.gd
@@ -1,28 +1,48 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const SPEED = 2.0
+const ACCEL = 4.0
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var nav_agent: NavigationAgent3D = $"../NavigationAgent3D"
+
+var update_timer = 0.0
 
 func _physics_process(delta: float) -> void:
-	# 1. Gravidade
+	# 1. Gravidade sempre ativa
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	if player:
-		# 2. Direção Direta (Ignore obstáculos por um momento para testar o FPS)
-		var direction = (player.global_position - global_position)
-		direction.y = 0
-		direction = direction.normalized()
-		
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		
-		# 3. Olhar para o player
-		var look_pos = player.global_position
-		look_pos.y = global_position.y
-		if global_position.distance_to(look_pos) > 0.5:
-			look_at(look_pos, Vector3.UP)
+	else:
+		velocity.y = 0
 
-	# 4. Move
+	if player and nav_agent:
+		# 2. Atualiza o destino apenas 5 vezes por segundo (Economiza CPU)
+		update_timer += delta
+		if update_timer >= 0.2:
+			nav_agent.target_position = player.global_position
+			update_timer = 0.0
+		
+		# 3. Calcula o movimento se ainda não chegou no alvo
+		if not nav_agent.is_navigation_finished():
+			var next_p = nav_agent.get_next_path_position()
+			var direction = (next_p - global_position)
+			
+			direction.y = 0 # FORÇA o inimigo a não subir
+			direction = direction.normalized()
+			
+			# Aplica a velocidade suavemente
+			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * ACCEL)
+			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * ACCEL)
+			
+			# 4. Rotação (Olha para o player, mas mantém o corpo reto)
+			var look_pos = player.global_position
+			look_pos.y = global_position.y
+			if global_position.distance_to(look_pos) > 0.5:
+				look_at(look_pos, Vector3.UP)
+		else:
+			# Para gradualmente ao chegar
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	# 5. Move o corpo físico
 	move_and_slide()
---
+++ red-valve/scripts/stages/stage_1.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@onready var navigation_region_3d: NavigationRegion3D = $chao/MeshInstance3D/NavigationRegion3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	navigation_region_3d.bake_navigation_mesh(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	navigation_region_3d.bake_navigation_mesh(true)
