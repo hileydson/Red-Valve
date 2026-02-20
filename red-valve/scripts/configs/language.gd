781174cdab1383c9198d11c573aa4c732008c568
hileydson <hileydson@hotmail.com>
2026-02-20 09:44:43 -0300

    Presets and language screen

---
+++ red-valve/scripts/configs/language.gd
@@ -0,0 +1,21 @@
+extends Node2D
+@onready var english: Button = $Control/VSplitContainer/english
+@onready var portuguese: Button = $Control/VSplitContainer/portuguese
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	english.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_english_pressed() -> void:
+	TranslationServer.set_locale("en_US")
+
+
+func _on_portuguese_pressed() -> void:
+	TranslationServer.set_locale("pt_BR")
