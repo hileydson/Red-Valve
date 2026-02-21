a2a2c174289ae69474e4d0544953e2740ebebb16
hileydson <hileydson@hotmail.com>
2026-02-21 11:11:38 -0300

    enemy blood

--- red-valve/scripts/enemies/blood.gd
+++ red-valve/scripts/enemies/blood.gd
@@ -1,9 +1,15 @@
 extends GPUParticles3D
+@onready var mancha: Sprite3D = $mancha
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	emitting = true
+	
+	mancha
+	var tween = create_tween()
+	tween.tween_property(mancha, "modulate:a", 0, 0.3) # Fade out
+	#tween.finished.connect(queue_free)
 	# Apaga o nó automaticamente após o efeito acabar
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()
