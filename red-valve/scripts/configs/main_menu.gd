908f0a34e2591899ebd6c37211912f0334339f17
hileydson <hileydson@hotmail.com>
2026-08-05 17:43:33 -0300

    casa terminada

--- red-valve/scripts/configs/main_menu.gd
+++ red-valve/scripts/configs/main_menu.gd
@@ -23,7 +23,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/stages/stage_1/stage_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/stages/prolog/the_house.tscn")
 
 
 func _on_exit_pressed() -> void:
