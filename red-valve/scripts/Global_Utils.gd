cd5a82110349c7e645d4dff4c9343598f7a6b6dd
hileydson <hileydson@hotmail.com>
2026-08-11 14:20:51 -0300

    ajuste bug reload

--- red-valve/scripts/Global_Utils.gd
+++ red-valve/scripts/Global_Utils.gd
@@ -1,45 +1,47 @@
 extends Node
 
 
+var current_time_tween: Tween = null
+
 func ativar_camera_lenta(escala: float, duracao: float, sound:bool):
-	# Muda a velocidade do tempo
 	Engine.time_scale = escala
 	
-	# som lento tambem
 	if sound:
 		AudioServer.set_playback_speed_scale(escala)
 	
-	# Cria um timer para voltar ao normal
-	# Usamos 'get_tree().create_timer' com o último parâmetro como 'true' 
-	# para que o próprio timer ignore a câmera lenta, senão ele demoraria para acabar!
 	await get_tree().create_timer(duracao * escala, true, false, true).timeout
 	
-	# Volta para a velocidade normal suavemente (opcional, mas fica mais bonito)
-	var tween = create_tween()
-	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS) # Faz o tween ignorar o time_scale
-	tween.tween_property(Engine, "time_scale", 1.0, 0.2)
+	if current_time_tween and current_time_tween.is_valid():
+		current_time_tween.kill()
+		
+	current_time_tween = create_tween()
+	current_time_tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	current_time_tween.tween_property(Engine, "time_scale", 1.0, 0.2)
+	
+	if sound:
+		current_time_tween.finished.connect(func():
+			AudioServer.set_playback_speed_scale(1.0)
+		)
 
 
 func ativar_camera_lenta_com_fim(escala: float, duracao: float, sound:bool):
-	# Muda a velocidade do tempo
 	Engine.time_scale = escala
 	
-	# som lento tambem
 	if sound:
 		AudioServer.set_playback_speed_scale(escala)
 	
-	# Cria um timer para voltar ao normal
-	# Usamos 'get_tree().create_timer' com o último parâmetro como 'true' 
-	# para que o próprio timer ignore a câmera lenta, senão ele demoraria para acabar!
 	await get_tree().create_timer(duracao * escala, true, false, true).timeout
 	
-	# Volta para a velocidade normal suavemente (opcional, mas fica mais bonito)
-	var tween = create_tween()
-	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS) # Faz o tween ignorar o time_scale
-	tween.tween_property(Engine, "time_scale", 1.0, 0.2)
-	tween.finished.connect(func(): 
+	if current_time_tween and current_time_tween.is_valid():
+		current_time_tween.kill()
+		
+	current_time_tween = create_tween()
+	current_time_tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	current_time_tween.tween_property(Engine, "time_scale", 1.0, 0.2)
+	current_time_tween.finished.connect(func(): 
 		remover_camera_lenta()
 	)
+	
 func remover_camera_lenta():
 	# 1. Volta a velocidade do motor ao normal imediatamente
 	Engine.time_scale = 1.0
