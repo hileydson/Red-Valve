b7f3a5770a305acb53703dc3d87ae89ef1a64091
hileydson <hileydson@hotmail.com>
2026-08-23 23:17:32 -0300

    Revert "trailer"
    This reverts commit 4dc1cc42990a78c7df956af8387c9efdc3c4fc47.

--- red-valve/scripts/stages/prolog/trailer_final.gd
+++ red-valve/scripts/stages/prolog/trailer_final.gd
@@ -4,8 +4,7 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var anti_lopes: Node3D = $the_anti_lopes
 
-const FLOATING_POSITION := Vector3(0, 1.2, 0)
-var skeleton_ref: Skeleton3D = null
+const TARGET_SCALE := Vector3(0.012, 0.012, 0.012)
 
 func _ready() -> void:
 	camera_final.make_current()
@@ -16,32 +15,22 @@ func _setup_anti_lopes() -> void:
 	if not is_instance_valid(anti_lopes):
 		return
 		
-	anti_lopes.position = FLOATING_POSITION
-	skeleton_ref = anti_lopes.find_child("GeneralSkeleton", true, false) as Skeleton3D
+	anti_lopes.scale = TARGET_SCALE
 	
+	# Busca o AnimationPlayer interno do modelo GLB e limpa as faixas de escala da animação
 	var internal_anim_player: AnimationPlayer = anti_lopes.find_child("AnimationPlayer", true, false) as AnimationPlayer
 	if internal_anim_player:
-		internal_anim_player.stop()
+		for anim_name in internal_anim_player.get_animation_list():
+			var anim = internal_anim_player.get_animation(anim_name)
+			if anim:
+				for i in range(anim.get_track_count() - 1, -1, -1):
+					if anim.track_get_type(i) == Animation.TYPE_SCALE_3D:
+						anim.remove_track(i)
 		
-		# Percorre todas as bibliotecas de animação e remove faixas de posição 3D
-		for lib_name in internal_anim_player.get_animation_library_list():
-			var lib = internal_anim_player.get_animation_library(lib_name)
-			if lib:
-				for anim_name in lib.get_animation_list():
-					var anim = lib.get_animation(anim_name)
-					if anim:
-						for i in range(anim.get_track_count() - 1, -1, -1):
-							if anim.track_get_type(i) == Animation.TYPE_POSITION_3D:
-								anim.remove_track(i)
-		
-		internal_anim_player.clear_caches()
 		if internal_anim_player.has_animation("Spear_Walk"):
+			internal_anim_player.autoplay = "Spear_Walk"
 			internal_anim_player.play("Spear_Walk")
 
 func _process(_delta: float) -> void:
 	if is_instance_valid(anti_lopes):
-		anti_lopes.position = FLOATING_POSITION
-		
-		# Trava a posição da bacia (osso 0 / Hips) no esqueleto para impedir qualquer deslocamento acumulado no loop
-		if is_instance_valid(skeleton_ref) and skeleton_ref.get_bone_count() > 0:
-			skeleton_ref.set_bone_pose_position(0, Vector3.ZERO)
+		anti_lopes.scale = TARGET_SCALE
